eaa0d6e4d364b0e35091e2bdda28772b441e97e3
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-08-18 05:44:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function map(arr, cb){
 function reduce(arr, cb, init=0){
   let total = init 
   arr.forEach((el)=>{
-    total = cb(total,el) 
+    return total = cb(total,el) 
   })
   return total
 }
