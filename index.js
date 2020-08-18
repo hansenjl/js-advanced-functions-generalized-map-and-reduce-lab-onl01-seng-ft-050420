c74e82b325b9d4c7ba304c1a112e36dc58359733
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-08-18 05:53:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(arr, cb){
+  let newArr = []
+  arr.forEach( (el) => {
+    newArr.push(cb(el))
+  })
+  return newArr
+}
+
+function reduce(arr, cb, init){
+  let total = arr[0]
+  if(init){
+    total = init 
+    for(let i=0; i < arr.length; i++){
+      total = cb(total,arr[i]) 
+    }
+  }else{
+    for(let i=1; i < arr.length; i++){
+      total = cb(total,arr[i]) 
+    }
+  }
+  
+  
+  return total
+}
