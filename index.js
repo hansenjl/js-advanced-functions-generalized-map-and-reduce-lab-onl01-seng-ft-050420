cb681c28fb7826df84813fe46308434405ba814f
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-08-18 05:45:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function map(arr, cb){
 
 function reduce(arr, cb, init=0){
   let total = init 
-  for(let i = 0; i < arr.length, i++){
+  for(let i=0; i < arr.length; i++){
     return total = cb(total,arr[i]) 
   }
   return total
